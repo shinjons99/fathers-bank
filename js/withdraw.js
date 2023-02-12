68c017dde98a3eeff52dad96d7b617920e3c9e6c
shinjons99 <shinjons99@gmail.com>
2023-02-13 01:05:30 +0600

    fixed bug

--- js/withdraw.js
+++ js/withdraw.js
@@ -20,22 +20,34 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    //* step-7
+    withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
     //* step-3:
    const withdrawTotalElement = document.getElementById('withdraw-amount');
    const previousWithdrawTotalString = withdrawTotalElement.innerText;
    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
    
-   //* step-4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+   
 
     //* step-5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+     
+    if(newWithdrawAmount> previousBalanceTotal){
+        alert('Baap er bank e Taka nai');
+        return;
+    }
+    //* step-4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
     //* step-6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-    //* step-7
-    withdrawField.value = '';
+   
 })
